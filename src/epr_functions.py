236474cf549996e4c3f3cb2af5aa285b95450f72
Mark Ian Braun <maku@marks-mac-mini.speedport_w_921v_1_48_000>
2023-11-16 16:10:33 +0100

    todo documentation

--- src/epr_functions.py
+++ src/epr_functions.py
@@ -16,6 +16,9 @@ def decimal_to_octal(num: int) -> str:
     return decimal_to_basis(num, 8)
 
 
+# Testcases
+
+
 def decimal_to_basis(num: int, basis: int) -> str:
     '''
     Converts a decimal number to a number of a given basis
@@ -23,19 +26,23 @@ def decimal_to_basis(num: int, basis: int) -> str:
     if num == 0:
         return '0'
     res = ''
+    conversion_map = {0: '0', 1: '1', 2: '2', 3: '3',
+                      4: '4', 5: '5', 6: '6', 7: '7',
+                      8: '8', 9: '9', 10: 'A', 11: 'B',
+                      12: 'C', 13: 'D', 14: 'E', 15: 'F'}
     while num != 0:
-        r = num % basis
+        r = conversion_map[num % basis]
         num //= basis
         res = str(r) + res
     return res
+# Testcases
 
 
 def chaos_turtle(i: int, x: int, y: int) -> None:
     '''
-    Draws with turtle module
+    Draws dots with turtle module. In each step the turtle moves halfway to one of three points
     '''
     triangle = ((10, 20), (0, 100), (100, 0))
-    print(type(triangle))
     t = turtle.Turtle()
     t.penup()
     t.setpos(x, y)
@@ -53,6 +60,11 @@ def chaos_turtle(i: int, x: int, y: int) -> None:
 
 
 if __name__ == '__main__':
-    print(decimal_to_octal(10))
-    print(decimal_to_basis(10, 2))
-    chaos_turtle(10, 0, 0)
+    print('decimal_to_octal')
+    print(decimal_to_octal(10))  # 12
+    print(decimal_to_octal(8))  # 10
+    print(decimal_to_octal(0))  # 0
+    print('decimal_to_basis')
+    print(decimal_to_basis(10, 2))  # 1010
+    print(decimal_to_basis(10, 8))  # 12
+    print(decimal_to_basis(170, 16))  # AA
