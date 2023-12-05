2f7471a5093446e8d90c1dfce6e8c26ce892b35a
Mark Ian Braun <maku@marks-mac-mini.speedport_w_921v_1_48_000>
2023-12-05 20:50:25 +0100

    gpr 7

--- src/GPR/GPR_ue07.py
+++ src/GPR/GPR_ue07.py
@@ -14,19 +14,16 @@ def get_best_students(students: dict) -> dict:
     best_grades = {}
     for key, value in students.items():
         if value[0] == 'EPI':
-            sum_of_grades = sum(value[1]) / len(value[1])
+            average = sum(value[1]) / len(value[1])
             if len(best_grades) < 3:
-                best_grades[key] = sum_of_grades
+                best_grades[key] = average
             else:
-                if sum_of_grades > any(value for value in best_grades.values()):
-                    min_grade = min(best_grades.values())
-                    print('min_grade', min_grade)
-                    print('best', best_grades)
-                    min_grade_key = min(
+                worst_grade = max(best_grades.values())
+                if average < worst_grade:
+                    worst_grade_key = min(
                         best_grades, key=lambda k: best_grades[k])
-                    print('min_grade_key', min_grade_key)
-                    best_grades.pop(min_grade_key)
-                    best_grades[key] = sum_of_grades
+                    best_grades.pop(worst_grade_key)
+                    best_grades[key] = average
     best_students = {key: students[key] for key in best_grades}
     return best_students
 
@@ -39,7 +36,7 @@ if __name__ == '__main__':
                   'hans2': ['EPI', (5, 2, 2, 4, 5)],
                   'hans3': ['EPI', (6, 2, 1, 4, 5)],
                   'hans4': ['EPI', (6, 2, 4, 4, 5)],
-                  'hans5': ['EPI', (6, 1, 2, 4, 5)],
-                  'hans6': ['EPI', (6, 1, 5, 4, 5)],
-                  'hans7': ['EPI', (6, 2, 3, 5, 6)]}
+                  'hans5': ['EPI', (1, 1, 2, 4, 5)],
+                  'hans6': ['EPI', (1, 1, 1, 1, 1)],
+                  'hans7': ['EPI', (1, 2, 3, 5, 6)]}
     print(get_best_students(input_dict))
