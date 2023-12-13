22d6a740e79bacf08c0e4ff9dbc68b9adddd74f6
Mark Ian Braun <maku@marks-mac-mini.speedport_w_921v_1_48_000>
2023-12-13 20:01:03 +0100

    almost

--- src/EPR/ue07.py
+++ src/EPR/ue07.py
@@ -1,3 +1,6 @@
+import copy
+
+
 def greedy_traversal(matrix):
     '''
     Traversal through a matrix with a greedy algorithm
@@ -26,6 +29,31 @@ def greedy_traversal(matrix):
     return way
 
 
+def find_best_way(matrix, i=0, j=0, position=[]):
+    # Base Case
+    print('matrix', matrix, 'i', i, 'j', j)
+    if i == len(matrix) - 1 and j == len(matrix[i]):
+        return matrix[i][j]
+    if i < 0 or j < 0:
+        return 0
+    if i >= len(matrix) or j >= len(matrix[i]):
+        return 0
+    cost = matrix[i][j]
+    position.append((i, j))
+    # reached endpoint
+
+    # Recursive Case
+    matrix[i].pop(j)
+    down = cost + find_best_way(matrix, i+1, j, position)
+    up = cost + find_best_way(matrix, i-1, j, position)
+    left = cost + find_best_way(matrix, i, j - 1, position)
+    right = cost + find_best_way(matrix, i, j + 1, position)
+
+    return min(down, up, left, right)
+
+
 if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
+    # import doctest
+    # doctest.testmod()
+    matrix = [[4, 0, 8], [-3, -4, 7], [-8, -1, 7]]
+    print(find_best_way(matrix))
