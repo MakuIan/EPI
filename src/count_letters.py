a682cafccf75693bbea7a0206275bf3dd853295d
Mark Ian Braun <maku@marks-mac-mini.speedport_w_921v_1_48_000>
2024-01-18 21:05:55 +0100

    gpr table 11

--- src/count_letters.py
+++ src/count_letters.py
@@ -8,14 +8,14 @@ import math
 def count_letters(text):
     """Count the number of times each letter occurs in text."""
     counts = {}
-    for letter in text:
+    for letter in text.lower():
         if letter in counts:
             counts[letter][0] += 1
         else:
             counts[letter] = [1]
     for letter, count in counts.items():
         count.append(round(count[0] / len(text), 3))
-        def entropie_formula(x): return -1 * count[1] * math.log2(count[1])
+        def entropie_formula(x): return -1 * x * math.log2(x)
         count.append(round(entropie_formula(count[1]), 3))
     print(counts)
     return counts
@@ -25,7 +25,6 @@ def display_counts(counts):
     """Display the letter counts in a text file."""
     header = ('Letter', 'Absolute', 'Relative', 'Entropie')
     letters = sorted(counts.keys())
-
     with open('table_GPR11.txt', 'w', encoding='utf-8') as file:
         # Write the header row
         file.write('{:<7}{:>10}{:>10}{:>10}\n'.format(*header))
@@ -34,6 +33,10 @@ def display_counts(counts):
         for letter in letters:
             file.write('{:<7}{:>10}{:>10}{:>10}\n'.format(
                 letter, *counts[letter]))
+    s = 0
+    for letter in counts:
+        s += counts[letter][1]
+    print(s)
 
 
 if __name__ == '__main__':
